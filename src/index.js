8b6dd2c2ecca5fe0f1b7581c8c1d3dfa13adf719
Amit Kumar Prajapati <35345631+AmitKumarPrajapati@users.noreply.github.com>
2019-06-27 02:08:27 +0530

    Remove the blank space

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ClickButton from "./ClickButton";
 import HoverCounter from "./HoverCounter";
-
 import "./styles.css";
 
 function App() {
